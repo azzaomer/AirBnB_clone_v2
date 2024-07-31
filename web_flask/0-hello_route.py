97ac9832a064714e3ef61e08f46364bc3f06bbbb
azzaomer <azzaomer70@gmail.com>
2024-07-31 15:22:36 -0700

    init

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False))
-deff hello_world():
+def hello_world():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
