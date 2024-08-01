24bf311d1a7f5948dfc6ec9d067c2a49589ecfc9
azzaomer <azzaomer70@gmail.com>
2024-08-02 01:25:37 +0300

    states list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,7 +8,7 @@ from models import storage
 app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
-def fetch_states()
+def fetch_states():
     """ list of all State objects present in DBStorage sorted by name"""
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
