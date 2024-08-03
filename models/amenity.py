b97e8a03e346a4d375ee1538b9f24fdb3703725e
azzaomer <azzaomer70@gmail.com>
2024-08-03 13:43:56 +0300

    places

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 """ Amenity Module for HBNB project """
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
     """Amenity class for HBNB project"""
-    if models.storage_t == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
-    place_amenities = relationship("Place",
-                                   secondary='place_amenity',
-                                   back_populates="amenities")
-    places = relationship("Place", secondary=place_amenity, back_populates="amenities")
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
