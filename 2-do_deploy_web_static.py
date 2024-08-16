0b2ab4302972822cb37bb703a60b573ae1137246
azzaomer <azzaomer70@gmail.com>
2024-08-16 16:51:19 +0300

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.207.227.199', '18.207.112.145']
+env.hosts = ['35.174.184.17', '54.164.112.145']
 
 def do_deploy(archive_path):
     """Deploys the static files to the host servers.
