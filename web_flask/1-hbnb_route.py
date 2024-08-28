7cf06ac1cc1539824382b748d35e834c7728db3b
azzaomer <azzaomer70@gmail.com>
2024-08-28 15:18:30 +0300

    number template

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,6 +12,8 @@ def hello_world():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
+    
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -43,6 +43,11 @@ def is_numbertemplate(n):
     """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_html(n):
+    """display HTML is "n" is a number only"""
+    return render_template('5-number.html', name=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
